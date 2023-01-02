d91f3693ff9a81bb4935b11eca89490f2ed610ea
TotoSpark <matheoroma@hotmail.fr>
2023-01-02 20:14:01 +0100

    Avencelent upload d'image sur la page d'acceuil

--- src/Controller/Gacceuil.php
+++ src/Controller/Gacceuil.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 
+use App\Entity\Article;
+use App\Form\ArticleType;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -52,6 +55,56 @@ class Gacceuil extends AbstractController
     public function test(){
         return $this->render('test.html.twig');
     }
+
+    public function add(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setLastUpdateDate(new \DateTime());
+
+            if ($article->getPicture() !== null) {
+                $file = $form->get('picture')->getData();
+                $fileName =  uniqid(). '.' .$file->guessExtension();
+
+                try {
+                    $file->move(
+                        $this->getParameter('images_directory'), // Le dossier dans lequel le fichier va être charger
+                        $fileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                $article->setPicture($fileName);
+            }
+
+            if ($article->getIsPublished()) {
+                $article->setPublicationDate(new \DateTime());
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return new Response('L\'article a bien été enregistrer.');
+        }
+
+        return $this->render('blog/add.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    public function edit($id)
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        return $this->render('blog/edit.html.twig', [
+            'article' => $article
+        ]);
+    }
+
 
     }
 
